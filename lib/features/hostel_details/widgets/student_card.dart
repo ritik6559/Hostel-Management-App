e84cde511e2f4d80bfa74a47fa1a1de6b3817ae6
Ritik6559 <ritikjoshi741@gmail.com>
2024-08-27 00:38:01 +0530

    student details screen completed

--- lib/features/hostel_details/widgets/student_card.dart
+++ lib/features/hostel_details/widgets/student_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_and_excel/models/student_model.dart';
+import 'package:clipboard/clipboard.dart';
 
 class StudentCard extends StatelessWidget {
   final Student student;
@@ -15,27 +16,42 @@ class StudentCard extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(12.0),
       child: Container(
-          padding: const EdgeInsets.all(15),
-          decoration: BoxDecoration(
-              color: color, borderRadius: BorderRadius.circular(20)),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                student.name,
-                style: TextStyle(
-                    color: Color(0xFF1f3854),
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            color: color, borderRadius: BorderRadius.circular(20)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              student.name,
+              style: TextStyle(
+                  color: Color(0xFF1f3854),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text('Roll No: ${student.rollNo}'),
+            Text('Room: ${student.roomNo}'),
+            Text('Floor: ${student.floor}'),
+            Text('Hostel: ${student.hostel}'),
+            GestureDetector(
+              onLongPress: () {
+                FlutterClipboard.copy(student.studentPhoneNo);
+              },
+              child: Text(
+                'Student\' phone no: ${student.studentPhoneNo}',
               ),
-              Text('Roll No: ${student.rollNo}'),
-              Text('Room: ${student.roomNo}'),
-              Text('Floor: ${student.floor}'),
-              Text('Hostel: ${student.hostel}'),
-              Text('Student\' phone no: ${student.studentPhoneNo}'),
-              Text('Parents\' phone no: ${student.parentPhoneNo}'),
-            ],
-          )),
+            ),
+            GestureDetector(
+              onLongPress: () {
+                FlutterClipboard.copy(student.studentPhoneNo);
+              },
+              child: Text(
+                'Parents\' phone no: ${student.parentPhoneNo}',
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
